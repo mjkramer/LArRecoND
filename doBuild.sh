de00f45a16e7b5f96423e75f87b95de9ed0c3eb8
John Back <J.J.Back@warwick.ac.uk>
2024-07-03 17:06:45 +0100

    Add libtorch & protobuf to CMAKE_PREFIX_PATH for doBuild.sh (DLVtx)

--- doBuild.sh
+++ doBuild.sh
@@ -49,15 +49,13 @@ cd LArContent
 git checkout $PANDORA_LAR_CONTENT_VERSION
 mkdir build
 cd build
-# Need to include cvmfs versions of compatible libtorch & protobuf for DL vertexing.
-# If you need to specify the cmake locations of these, then add the following cmake flag:
-#-DCMAKE_PREFIX_PATH="/cvmfs/larsoft.opensciencegrid.org/products/libtorch/v1_6_0d/Linux64bit+3.10-2.17-e20/share/cmake;/cvmfs/larsoft.opensciencegrid.org/products/protobuf/v3_12_3a/Linux64bit+3.10-2.17-e20/lib/cmake/protobuf"
-
+# Need to include cvmfs versions of compatible libtorch & protobuf for DL vertexing
 cmake -DCMAKE_CXX_STANDARD=17 \
 -DCMAKE_MODULE_PATH="$MY_TEST_AREA/PandoraPFA/cmakemodules;$ROOTSYS/etc/cmake" \
 -DPANDORA_MONITORING=ON -DPandoraSDK_DIR=$MY_TEST_AREA/PandoraSDK \
 -DPandoraMonitoring_DIR=$MY_TEST_AREA/PandoraMonitoring \
 -DPANDORA_LIBTORCH=ON \
+-DCMAKE_PREFIX_PATH="/cvmfs/larsoft.opensciencegrid.org/products/libtorch/v1_6_0d/Linux64bit+3.10-2.17-e20/share/cmake;/cvmfs/larsoft.opensciencegrid.org/products/protobuf/v3_12_3a/Linux64bit+3.10-2.17-e20/lib/cmake/protobuf" \
 -DEigen3_DIR=$MY_TEST_AREA/Eigen3/share/eigen3/cmake/ ..
 make -j4 install
 
@@ -71,7 +69,7 @@ export CMAKE_PREFIX_PATH=${EDEP_ROOT}/${EDEP_TARGET}
 mkdir -p install
 cd build
 cmake -DCMAKE_INSTALL_PREFIX=${EDEP_ROOT}/install \
--DGeant4_DIR=/cvmfs/larsoft.opensciencegrid.org/products/geant4/v4_10_6_p01c/Linux64bit+3.10-2.17-e19-prof/lib64/Geant4-10.6.1 ..
+-DGeant4_DIR="/cvmfs/larsoft.opensciencegrid.org/products/geant4/v4_10_6_p01c/Linux64bit+3.10-2.17-e19-prof/lib64/Geant4-10.6.1" ..
 make -j4 install
 
 
@@ -82,14 +80,13 @@ cd LArRecoND
 git checkout master
 mkdir build
 cd build
-# Need to specify LArContent, LArDLContent, Geant4, ROOT & edep-sim. May also need libtorch & protobuf:
-#-DCMAKE_PREFIX_PATH="/cvmfs/larsoft.opensciencegrid.org/products/libtorch/v1_6_0d/Linux64bit+3.10-2.17-e20/share/cmake;/cvmfs/larsoft.opensciencegrid.org/products/protobuf/v3_12_3a/Linux64bit+3.10-2.17-e20/lib/cmake/protobuf"
-
+# Need to specify LArContent, LArDLContent, Geant4, ROOT & edep-sim. Also need libtorch & protobuf for DLVtx
 cmake -DCMAKE_MODULE_PATH="$MY_TEST_AREA/PandoraPFA/cmakemodules;$ROOTSYS/etc/cmake" \
 -DPANDORA_MONITORING=ON -DPandoraSDK_DIR=$MY_TEST_AREA/PandoraSDK/ \
 -DPANDORA_LIBTORCH=ON -DPandoraMonitoring_DIR=$MY_TEST_AREA/PandoraMonitoring/ \
 -DLArContent_DIR=$MY_TEST_AREA/LArContent/ -DLArDLContent_DIR=$MY_TEST_AREA/LArContent/ \
--DGeant4_DIR=/cvmfs/larsoft.opensciencegrid.org/products/geant4/v4_10_6_p01c/Linux64bit+3.10-2.17-e19-prof/lib64/Geant4-10.6.1 \
+-DGeant4_DIR="/cvmfs/larsoft.opensciencegrid.org/products/geant4/v4_10_6_p01c/Linux64bit+3.10-2.17-e19-prof/lib64/Geant4-10.6.1" \
+-DCMAKE_PREFIX_PATH="/cvmfs/larsoft.opensciencegrid.org/products/libtorch/v1_6_0d/Linux64bit+3.10-2.17-e20/share/cmake;/cvmfs/larsoft.opensciencegrid.org/products/protobuf/v3_12_3a/Linux64bit+3.10-2.17-e20/lib/cmake/protobuf" \
 -DEDepSim_DIR=$MY_TEST_AREA/edep-sim/build ..
 make -j4 install
 
